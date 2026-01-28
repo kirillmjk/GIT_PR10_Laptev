ff7ee6fdfd24a7cfa8e49d22b80d59c308eebc2f
Laptev <vasilev@example.com>
2026-01-28 11:45:09 +0300

    Изменение Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine($"date: {DateTime.Now}");
+Console.WriteLine("Группа: ИС-22Б");
+Console.WriteLine("Имя: Лаптев К.С.");
